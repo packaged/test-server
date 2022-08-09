8cc87293fc33f4a002f4bc45318011f398cea889
Tom Kay <oridan82@gmail.com>
2022-08-09 09:52:36 +0100

    support certificate

--- server.go
+++ server.go
@@ -11,6 +11,8 @@ var (
 	port      = cli.Flag("port", "port").Short('p').String()
 	servePath = cli.Flag("servePath", "serve path").Short('s').Default("./static").String()
 	baseURL   = cli.Flag("baseUrl", "base url").Short('b').Default("/").String()
+	certFile  = cli.Flag("cert", "certificate").Short('c').Default("").String()
+	certKey   = cli.Flag("key", "certificate key").Short('k').Default("").String()
 )
 
 func main() {
@@ -23,5 +25,9 @@ func main() {
 	http.Handle(*baseURL, http.StripPrefix(*baseURL, http.FileServer(http.Dir(*servePath))))
 
 	log.Printf("Listening on http://0.0.0.0:" + *port)
-	log.Fatal(http.ListenAndServe(":"+*port, nil))
+	if *certFile != "" && *certKey != "" {
+		log.Fatal(http.ListenAndServeTLS(":"+*port, *certFile, *certKey, nil))
+	} else {
+		log.Fatal(http.ListenAndServe(":"+*port, nil))
+	}
 }
